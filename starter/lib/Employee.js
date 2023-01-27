1e778d512c90889f2c4d419cb73a807783a42514
Nick <72912456+nickmbk@users.noreply.github.com>
2023-01-27 19:26:03 +0000

    edit manager and employee constructors

--- starter/lib/Employee.js
+++ starter/lib/Employee.js
@@ -1,9 +1,9 @@
 // TODO: Write code to define and export the Employee class
 class Employee {
-    constructor() {
-        this.name = getName();
-        this.id = getId();
-        this.email = getEmail();
+    constructor(name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
     }
 
     // getName() {
--- starter/lib/Manager.js
+++ starter/lib/Manager.js
@@ -3,8 +3,8 @@ const Employee = require('./Employee');
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
 
 class Manager extends Employee {
-    constructor() {
-        super(id, officeNumber);
+    constructor(officeNumber) {
+        super(name, id, email);
         this.officeNumber = officeNumber;
     }
 
