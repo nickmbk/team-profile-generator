1d3ca665167b82ab666531d29f780048d45b657e
Nick <72912456+nickmbk@users.noreply.github.com>
2023-01-27 19:58:06 +0000

    create inquirer prompts for manager

--- starter/index.js
+++ starter/index.js
@@ -13,3 +13,55 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+function createManager() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'Please enter the Team Manager\'s name: ',
+                name: 'managersName'
+            },
+            {
+                type: 'input',
+                message: 'Please enter the Manager\'s ID: ',
+                name: 'managersId'
+            },
+            {
+                type: 'input',
+                message: 'Please enter the Manager\'s email address: ',
+                name: 'managersEmail'
+            },
+            {
+                type: 'input',
+                message: 'Please enter the Manager\'s Office number: ',
+                name: 'managersOfficeNo'
+            }
+        ])
+        .then((answers) => {
+            
+        })
+} 
+
+
+
+
+// When a user starts the application then they are prompted to enter the team manager’s:
+// Name
+// Employee ID
+// Email address
+// Office number
+// When a user enters those requirements then the user is presented with a menu with the option to:
+// Add an engineer
+// Add an intern
+// Finish building the team
+// When a user selects the engineer option then a user is prompted to enter the following and then the user is taken back to the menu:
+// Engineer's Name
+// ID
+// Email
+// GitHub username
+// When a user selects the intern option then a user is prompted to enter the following and then the user is taken back to the menu:
+// Intern’s name
+// ID
+// Email
+// School
+// When a user decides to finish building their team then they exit the application, and the HTML is generated.
