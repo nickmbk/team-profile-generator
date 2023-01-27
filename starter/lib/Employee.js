205db5af1528841261e87e8db757827117eeadf4
Nick <72912456+nickmbk@users.noreply.github.com>
2023-01-27 18:42:49 +0000

    create Employee constructor

--- starter/lib/Employee.js
+++ starter/lib/Employee.js
@@ -1,1 +1,10 @@
 // TODO: Write code to define and export the Employee class
+class Employee {
+    constructor() {
+        this.name = getName();
+        this.id = getId();
+        this.email = getEmail();
+        this.role = getRole();  //returns Employee
+    }
+
+}
